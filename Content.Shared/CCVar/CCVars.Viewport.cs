2bc44be7f91b00ddc79e56234b3e25ee36335dba
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2026-04-27 18:14:12 -0400

    make viewport square

--- Content.Shared/CCVar/CCVars.Viewport.cs
+++ Content.Shared/CCVar/CCVars.Viewport.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.Configuration;
+using Robust.Shared.Configuration;
 
 namespace Content.Shared.CCVar;
 
@@ -23,7 +23,7 @@ public sealed partial class CCVars
         CVarDef.Create("viewport.minimum_width", 15, CVar.REPLICATED | CVar.SERVER);
 
     public static readonly CVarDef<int> ViewportMaximumWidth =
-        CVarDef.Create("viewport.maximum_width", 21, CVar.REPLICATED | CVar.SERVER);
+        CVarDef.Create("viewport.maximum_width", 15, CVar.REPLICATED | CVar.SERVER); // FH
 
     public static readonly CVarDef<int> ViewportWidth =
         CVarDef.Create("viewport.width", 21, CVar.CLIENTONLY | CVar.ARCHIVE);
