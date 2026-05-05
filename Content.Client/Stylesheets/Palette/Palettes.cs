fa25404846ce048dfeacf94573f142cc14fdb051
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2026-05-04 20:14:19 -0400

    new UI

--- Content.Client/Stylesheets/Palette/Palettes.cs
+++ Content.Client/Stylesheets/Palette/Palettes.cs
@@ -9,10 +9,10 @@ namespace Content.Client.Stylesheets.Palette;
 public static class Palettes
 {
     // muted tones
-    public static readonly ColorPalette Navy = ColorPalette.FromHexBase("#c64407", lightnessShift: 0.05f, chromaShift: 0.0045f); // FH start
-    public static readonly ColorPalette Cyan = ColorPalette.FromHexBase("#9b4218", lightnessShift: 0.05f, chromaShift: 0.0045f);
-    public static readonly ColorPalette Slate = ColorPalette.FromHexBase("#282524");
-    public static readonly ColorPalette Neutral = ColorPalette.FromHexBase("#000000"); // FH end
+    public static readonly ColorPalette Navy = ColorPalette.FromHexBase("#364927", lightnessShift: 0.05f, chromaShift: 0.0045f); // FH start
+    public static readonly ColorPalette Cyan = ColorPalette.FromHexBase("#283022", lightnessShift: 0.05f, chromaShift: 0.0045f);
+    public static readonly ColorPalette Slate = ColorPalette.FromHexBase("#1f231c");
+    public static readonly ColorPalette Neutral = ColorPalette.FromHexBase("#3a3d38"); // FH end
 
     // status tones
     public static readonly ColorPalette Red = ColorPalette.FromHexBase("#b62124", chromaShift: 0.02f);
@@ -21,7 +21,7 @@ public static class Palettes
     public static readonly StatusPalette Status = new([Red.Base, Amber.Base, Green.Base]);
 
     // highlight tones
-    public static readonly ColorPalette Gold = ColorPalette.FromHexBase("#db360d"); // FH
+    public static readonly ColorPalette Gold = ColorPalette.FromHexBase("#8e3434"); // FH
     public static readonly ColorPalette Maroon = ColorPalette.FromHexBase("#9b2236");
 
     // Intended to be used with `ModulateSelf` to darken / lighten something
