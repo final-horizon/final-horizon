2db65638311304f94afd0b0139a1dde913115caa
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2026-05-27 12:41:47 -0400

    portals

---
+++ Content.Server/_Stalker_EN/Teleportation/LinkByStringSystem.cs
@@ -0,0 +1,69 @@
+using Content.Server.SubFloor;
+using Content.Shared.Random.Rules;
+using Content.Shared.Teleportation.Components;
+using Content.Shared.Teleportation.Systems;
+using NetCord;
+using Robust.Shared.GameStates;
+using Robust.Shared.Physics.Events;
+
+namespace Content.Server._Stalker_EN.Teleportation;
+
+/// <summary>
+/// This is used for Linking portals via a common string
+/// </summary>
+public sealed class LinkByStringSystem : EntitySystem
+{
+    [Dependency] private readonly LinkedEntitySystem _link = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<LinkByStringComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(Entity<LinkByStringComponent> ent, ref MapInitEvent args)
+    {
+        TryLink(ent);
+    }
+
+    private void TryLink(Entity<LinkByStringComponent> ent)
+    {
+        // If we don't have any valid way of linking, then return
+        var linkString = EvaluateLinkString(ent);
+        if (linkString == null)
+            return;
+
+        var query = EntityQueryEnumerator<LinkByStringComponent>();
+
+        while (query.MoveNext(out var uid, out var link))
+        {
+            if (ent.Owner == uid)
+                continue;
+
+            var otherLinkString = EvaluateLinkString((uid, link));
+
+            if (otherLinkString == null)
+                continue;
+
+            if (otherLinkString != linkString)
+                continue;
+
+            _link.TryLink(ent.Owner, uid);
+        }
+    }
+
+    private string? EvaluateLinkString(Entity<LinkByStringComponent> ent)
+    {
+        var linkString = ent.Comp.LinkString;
+
+        if (ent.Comp.FallbackId &&
+            linkString == null &&
+            MetaData(ent.Owner).EntityPrototype != null)
+        {
+            linkString = MetaData(ent.Owner).EntityPrototype?.ID;
+        }
+
+        return linkString;
+    }
+}
