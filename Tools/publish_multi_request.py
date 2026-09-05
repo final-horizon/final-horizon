a9378f9bbdcc70f3f6bce61df448d5d00ed49b41
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2026-09-05 18:07:04 -0400

    stupid evil typo

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://cdn.dallaszzz.org:8080"
+ROBUST_CDN_URL = "http://cdn.dallaszzz.org:8080/"
 FORK_ID = "finalhorizon"
 
 def main():
